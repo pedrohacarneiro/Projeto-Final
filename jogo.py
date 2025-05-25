feb83135044ed5e5a7f6cde7199d7a91488e8216
rafasemenovitch <rafasemenovitch@gmail.com>
2025-05-25 14:10:19 -0300

    commit

--- jogo.py
+++ jogo.py
@@ -1,47 +1,3 @@
-# import pygame
-# pygame.init()
-# from parametros import *
-# from funcoes import *
-
-# def main():
-#     # Configurações iniciais
-#     jogo = True
-#     tela_atual = "tela inicial"
-
-#     # Telas - cada uma com suas próprias dimensões
-#     TELA_INICIAL = pygame.display.set_mode((LARGURA, ALTURA))
-#     pygame.display.set_caption("Springfield Surfers")
-
-#     # Loop principal do jogo
-#     contador=0
-#     while jogo:
-#         if tela_atual == "tela inicial":
-#             jogo, tela_atual,contador = tela_inicial(
-#                 jogo, 
-#                 TELA_INICIAL,  # Usando apenas TELA_INICIAL aqui
-#                 fundo, 
-#                 titulo, 
-#                 titulo_rect, 
-#                 personagem, 
-#                 personagem_rect, 
-#                 botaojogar, 
-#                 botaojogar_rect, 
-#                 tela_atual,
-#                 contador
-#             )
-#         elif tela_atual == "tela jogo":
-#             # Criamos a tela de jogo apenas quando necessário
-#             TELA_JOGO = pygame.display.set_mode((WIDTH, HEIGHT))
-#             jogo, tela_atual,contador = tela_jogo(TELA_JOGO,contador)
-#             # Quando sair do jogo, voltamos para a tela inicial
-#             if tela_atual == "tela inicial":
-#                 TELA_INICIAL = pygame.display.set_mode((LARGURA, ALTURA))
-
-#     pygame.quit()
-
-# if __name__ == "__main__":
-#     main()
-
 import pygame
 pygame.init()
 from parametros import *
@@ -85,3 +41,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
